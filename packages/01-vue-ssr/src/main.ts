08ff67dab9c07b2885d98abd5d6dd9590e3b9f35
peggy <peggyio4142@gmail.com>
2020-10-13 16:46:36 +0800

    update:package version

--- packages/01-vue-ssr/src/main.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import createRouter from './router'
-import App from './App'
-const router=createRouter()
-new Vue({
-    el: '#app',
-    router,
-    render: h => h(App),
-});
